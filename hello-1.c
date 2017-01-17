b98fb43b240f4796d1298eaa4fc749c7517ddbc3
student <sherf.ofri@gmail.com>
2017-01-17 20:15:43 +0200

    Working. with random num function, reads two cards

--- hello-1.c
+++ hello-1.c
@@ -6,7 +6,9 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <asm/uaccess.h>	/* for put_user */
+#include <linux/random.h>
 
+int get_random_num(int max);
 int init_module(void);
 void cleanup_module(void);
 static int device_open(struct inode *, struct file *);
@@ -17,12 +19,32 @@ static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 #define DEVICE_NAME "cardsdev"
 static int Major;		/* Major number assigned to our device driver */
 
+static char nums[] = {
+   'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'
+};
+
+static char suits[] = {
+   'c', 's', 'd', 'h'
+};
+
+
 static struct file_operations fops = {
 	.read = device_read,
 	.write = device_write,
 	.open = device_open,
 	.release = device_release
 };
+
+int get_random_num(int max)
+{
+    unsigned int i;
+    int *ip;
+
+    ip = &i;
+    get_random_bytes(&i, sizeof i);
+
+    return (i%max);
+}
 
 static int device_open(struct inode *inode, struct file *file)
 {
@@ -45,10 +67,30 @@ static ssize_t device_read(struct file *filp,	/* see include/linux/fs.h   */
 			   size_t length,	/* length of the buffer     */
 			   loff_t * offset)
 {
-    put_user('9', buffer++);
-    put_user('H', buffer++);
+    printk(KERN_INFO "start");
 
-    return 2;
+    // Get two random numbers
+    int rand1 = get_random_num(4*13);
+    int rand2 = get_random_num(4*13);
+    
+    // In case the numbers are the same
+    while (rand1 == rand2)
+    {
+        rand2 = get_random_num(4*13);
+    }
+
+    int suit1 = rand1%4;
+    int num1 = rand1/4;
+    int suit2 = rand2%4;
+    int num2 = rand2/4;
+
+    put_user(nums[num1], buffer++);
+    put_user(suits[suit1], buffer++);
+    
+    put_user(nums[num2], buffer++);
+    put_user(suits[suit2], buffer++);
+
+    return 4;
 }
 
 /*
