a348a3e270f3eb180f8e7eaea5280bf2c2bce9fc
student <sherf.ofri@gmail.com>
2017-01-17 18:51:47 +0200

    Added read function which puts in the buffer 'b' to view dmesg: dmesg -wH
    load the module:
    sudo insmod hello-1.ko
    
    then in dmesg:
    I was assigned major number 246. To talk to
    [  +0.000008] the driver, create a dev file with
    [  +0.000004] 'mknod /dev/cardsdev c 246 0'.
    [  +0.000003] Try various minor numbers. Try to cat and echo to
    [  +0.000003] the device file.
    [  +0.000002] Remove the device file and module when done.
    
    mknod:
    sudo mknod /dev/cardsdev c 246 0
    
    read from the device:
    cat /dev/cardsdev
    
    remove the device:
    sudo rmmod hello-1.ko

--- hello-1.c
+++ hello-1.c
@@ -2,20 +2,85 @@
 /*  
  *  hello-1.c - The simplest kernel module.
  */
-#include <linux/module.h>	/* Needed by all modules */
-#include <linux/kernel.h>	/* Needed for KERN_INFO */
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>	/* for put_user */
 
+int init_module(void);
+void cleanup_module(void);
+static int device_open(struct inode *, struct file *);
+static int device_release(struct inode *, struct file *);
+static ssize_t device_read(struct file *, char *, size_t, loff_t *);
+static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
+
+#define DEVICE_NAME "cardsdev"
+static int Major;		/* Major number assigned to our device driver */
+
+static struct file_operations fops = {
+	.read = device_read,
+	.write = device_write,
+	.open = device_open,
+	.release = device_release
+};
+
+static int device_open(struct inode *inode, struct file *file)
+{
+    return 0;
+}
+
+static int device_release(struct inode *inode, struct file *file)
+{
+    return 0;
+}
+
+static ssize_t
+device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
+{
+    return 0;
+}
+
+static ssize_t device_read(struct file *filp,	/* see include/linux/fs.h   */
+			   char *buffer,	/* buffer to fill with data */
+			   size_t length,	/* length of the buffer     */
+			   loff_t * offset)
+{
+    put_user('b', buffer++);
+
+    return 1;
+}
+
+/*
+ * This function is called when the module is loaded
+ */
 int init_module(void)
 {
-	printk(KERN_INFO "Hello world 1.\n");
+        Major = register_chrdev(0, DEVICE_NAME, &fops);
 
-	/* 
-	 * A non 0 return means init_module failed; module can't be loaded. 
-	 */
+	if (Major < 0) {
+	  printk(KERN_ALERT "Registering char device failed with %d\n", Major);
+	  return Major;
+	}
+
+	printk(KERN_INFO "I was assigned major number %d. To talk to\n", Major);
+	printk(KERN_INFO "the driver, create a dev file with\n");
+	printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Major);
+	printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n");
+	printk(KERN_INFO "the device file.\n");
+	printk(KERN_INFO "Remove the device file and module when done.\n");
+
 	return 0;
 }
 
+/*
+ * This function is called when the module is unloaded
+ */
 void cleanup_module(void)
 {
-	printk(KERN_INFO "Goodbye world 1.\n");
+	/* 
+	 * Unregister the device 
+	 */
+	unregister_chrdev(Major, DEVICE_NAME);
 }
+
+
